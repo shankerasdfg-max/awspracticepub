82227dd2cf67c1f8802a1b74f6d089b4e311ab2f
shankerasdfg-max <shankerasdfg@gmail.com>
2025-10-07 01:53:02 +1100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('Hello from Node.js on GKE via Jenkins CI/CD!');
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8082;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
